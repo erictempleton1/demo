f53206e383f5fd4f860068d94fed5c25b55e8d3d
etempleton <erictempleton1@gmail.com>
2018-04-01 15:16:36 -0400

    working on networks page

---
+++ src/components/networks/NetworksTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <md-table md-card>
+      <md-table-toolbar>
+        <h1 class="md-title">Networks</h1>
+      </md-table-toolbar>
+
+      <md-table-row>
+        <md-table-head md-numeric>ID</md-table-head>
+        <md-table-head>Name</md-table-head>
+        <md-table-head>Created</md-table-head>
+        <md-table-head>Modified</md-table-head>
+        <md-table-head>Status</md-table-head>
+      </md-table-row>
+
+      <md-table-row>
+        <md-table-cell md-numeric>100</md-table-cell>
+        <md-table-cell>Cool network</md-table-cell>
+        <md-table-cell>3/31/2018</md-table-cell>
+        <md-table-cell>4/1/2018</md-table-cell>
+        <md-table-cell>Paused</md-table-cell>
+      </md-table-row>
+    </md-table>
+</template>
+
+<script>
+export default {
+  name: 'NetworksTable',
+};
+</script>
--- src/views/Networks.vue
+++ src/views/Networks.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="page-container">
         <nav-bar></nav-bar>
+        <networks-table></networks-table>
     </div>
 </template>
 
 <script>
 import NavBar from '@/components/navbar/NavBar';
+import NetworksTable from '@/components/networks/NetworksTable';
 
 export default {
   name: 'Networks',
-  components: { NavBar },
+  components: { NavBar, NetworksTable },
 };
 </script>
